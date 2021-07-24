90de6a7cbdc53e8ea461da875f9c9f855f591c73
cosmicBboy <niels.bantilan@gmail.com>
2021-07-24 16:52:17 -0400

    debuggin

--- scripts/run-grid-experiment.sh
+++ scripts/run-grid-experiment.sh
@@ -7,4 +7,7 @@ source env/gridai
 envsubst < gridai-config.yml > /tmp/gridai-config.yml
 grid run \
     --config /tmp/gridai-config.yml \
+    --datastore_name $GRID_DATASTORE_NAME \
+    --datastore_version $GRID_DATASTORE_VERSION \
+    --datastore_mount_dir $GRID_DATASTORE_MOUNT_DIR \
     movenet/trainer.py $@
