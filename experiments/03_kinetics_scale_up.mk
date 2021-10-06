9ca96e5cd187c4c7f63f968134434ba7d4ba7ba8
cosmicBboy <niels.bantilan@gmail.com>
2021-10-06 14:54:27 -0400

    add gpu dockerfile

--- experiments/03_kinetics_scale_up.mk
+++ experiments/03_kinetics_scale_up.mk
@@ -8,6 +8,14 @@ TRAIN_DEBUG_OPTS?=--dataset /opt/datastore \
 	--stack_size 3 \
 	--checkpoint_every 1
 
+DATASET_DEBUG_OPTS?=--datastore_name kinetics-debug \
+	--datastore_version 5 \
+	--datastore_mount_dir /opt/datastore \
+
+INFRA_DEBUG_OPTS?=--scratch_size 512 \
+	--memory 60G \
+	--framework torch \
+
 
 PRETRAINED_MODEL_PATH?=
 PRETRAINED_RUN_EXP_NAME?=
@@ -19,11 +27,19 @@ train-debug:
 	grid run --dockerfile Dockerfile \
 		--instance_type t2.2xlarge \
 		--cpus 7  \
-		--scratch_size 512 \
-		--memory 60G \
-		--framework torch \
-		--datastore_name kinetics-debug \
-		--datastore_version 5 \
-		--datastore_mount_dir /opt/datastore \
+		${INFRA_DEBUG_OPTS} \
+		${DATASET_DEBUG_OPTS} \
+		movenet/trainer.py ${TRAIN_DEBUG_OPTS} \
+			--wandb_api_key=${WANDB_API_KEY}
+
+
+.PHONY: train-debug-gpu
+train-debug:
+	grid run --dockerfile Dockerfile-gpu \
+		--instance_type p2.xlarge \
+		--cpus 3  \
+		--gpus 1 \
+		${INFRA_DEBUG_OPTS} \
+		${DATASET_DEBUG_OPTS} \
 		movenet/trainer.py ${TRAIN_DEBUG_OPTS} \
 			--wandb_api_key=${WANDB_API_KEY}
