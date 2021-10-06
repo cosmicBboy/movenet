fef31c53dfce19a639365ff5977d8c82938b1bd4
cosmicBboy <niels.bantilan@gmail.com>
2021-10-06 00:56:34 -0400

    add dev requirements.txt

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 with open("requirements.txt") as f:
     requires = list(x for x in f.readlines())
 
+with open("dev-requirements.txt") as f:
+    requires = list(x for x in f.readlines())
+
 setup(
     name="movenet",
     version="0.0.0+dev0",
