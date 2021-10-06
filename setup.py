a5f880a314d56a4d3b71679e6a79b50d943a3f6e
cosmicBboy <niels.bantilan@gmail.com>
2021-10-06 00:34:11 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,26 @@
 from setuptools import setup
 
 
+EXCLUDE_REQUIRES = {
+    "dask",
+    "joblib",
+    "tensorboard",
+    "tensorflow",
+    "typing-extensions==3.7.4.3",
+}
+
+
+with open("requirements.txt") as f:
+    requires = [
+        x for x in f.readlines() if x.split("==")[0] in EXCLUDE_REQUIRES
+    ]
+
+
 setup(
     name="movenet",
     version="0.0.0+dev0",
     author="Niels Bantilan",
     author_email="niels.bantilan@gmail.com",
     packages=["movenet"],
-    install_requires=[
-        "av",
-        "dask",
-        "dataclasses==0.6",
-        "dataclasses-json==0.5.2",
-        "joblib",
-        "librosa==0.8.1",
-        "numpy==1.20.3",
-        "opencv-python",
-        "opencv-python-headless==4.5.3.56",
-        "tensorboard",
-        "tensorflow",
-        "tqdm",
-        "torch==1.9.0",
-        "torchvision==0.10.0",
-        "torchaudio==0.9.0",
-        "torchtyping==0.1.2",
-        "typeguard",
-        "typing-extensions==3.7.4.3",
-        "ipdb==0.13.7",
-        "pytorchvideo==0.1.2",
-        "wandb",
-    ],
+    install_requires=requires,
 )
