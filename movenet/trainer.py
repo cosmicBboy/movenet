748a89cdbaa245324d29fbcdd85d00b27c204a52
cosmicBboy <niels.bantilan@gmail.com>
2021-10-08 13:59:06 -0400

    debugging

--- movenet/trainer.py
+++ movenet/trainer.py
@@ -73,8 +73,8 @@ class TrainingConfig:
 
 
 def training_step(model, optimizer, audio, video):
-    if torch.cuda.is_available():
-        audio, video = audio.to("cuda"), video.to("cuda")
+    # if torch.cuda.is_available():
+    #     audio, video = audio.to("cuda"), video.to("cuda")
     output = model(audio, video)
     target = audio[:, :, model.receptive_fields:].argmax(1)
     loss = F.cross_entropy(output, target)
@@ -93,8 +93,8 @@ def training_step(model, optimizer, audio, video):
 
 @torch.no_grad()
 def validation_step(model, audio, video):
-    if torch.cuda.is_available():
-        audio, video = audio.to("cuda"), video.to("cuda")
+    # if torch.cuda.is_available():
+    #     audio, video = audio.to("cuda"), video.to("cuda")
     # TODO: make sure this is only using video to generate audio
     # - need to figure out how to auto-regressively feed in the audio output to
     #   generate full audio sequence
