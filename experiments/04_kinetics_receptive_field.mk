c711bd804cd45e826b6959e402a3b928c7728369
cosmicBboy <niels.bantilan@gmail.com>
2022-01-25 17:27:47 -0500

    add experiments with increased receptive fields

---
+++ experiments/04_kinetics_receptive_field.mk
@@ -0,0 +1,114 @@
+# Experiments involving increased receptive field
+N_DEBUG_EPOCHS?=3
+
+TRAIN_DEBUG_OPTS?=--dataset /opt/datastore \
+	--n_epochs ${N_DEBUG_EPOCHS} \
+	--batch_size 2 \
+	--learning_rate 0.00003 \
+	--input_channels 128 \
+	--residual_channels 32 \
+	--layer_size 14 \
+	--stack_size 2 \
+	--checkpoint_every 1 \
+	--accumulation_steps 10
+
+DATASET_DEBUG_OPTS?=--datastore_name kinetics-debug \
+	--datastore_version 5 \
+	--datastore_mount_dir /opt/datastore
+
+DATASET_GPU_DEBUG_OPTS?=--datastore_name kinetics-breakdancing \
+	--datastore_version 2 \
+	--datastore_mount_dir /opt/datastore
+
+INFRA_DEBUG_OPTS?=--scratch_size 512 \
+	--memory 100
+
+
+PRETRAINED_RUN_EXP_NAME?=
+
+
+.PHONY: train-debug
+train-debug:
+	grid run --dockerfile Dockerfile \
+		--instance_type t2.2xlarge \
+		--cpus 7  \
+		--ignore_warnings \
+		${INFRA_DEBUG_OPTS} \
+		${DATASET_DEBUG_OPTS} \
+		movenet/trainer.py ${TRAIN_DEBUG_OPTS} \
+			--model_output_path models \
+			--wandb_api_key=${WANDB_API_KEY}
+
+
+.PHONY: train-debug-gpu
+train-debug-gpu:
+	grid run --dockerfile Dockerfile-gpu \
+		--instance_type p3.8xlarge \
+		--cpus 30  \
+		--gpus 4 \
+		--ignore_warnings \
+		${INFRA_DEBUG_OPTS} \
+		${DATASET_GPU_DEBUG_OPTS} \
+		movenet/trainer.py ${TRAIN_DEBUG_OPTS} \
+			--model_output_path models \
+			--wandb_api_key=${WANDB_API_KEY}
+
+
+N_EPOCHS?=3
+
+TRAIN_OPTS?=--dataset /opt/datastore \
+	--n_epochs ${N_EPOCHS} \
+	--batch_size 3 \
+	--learning_rate 0.00003 \
+	--pin_memory 1 \
+	--num_workers 4 \
+	--input_channels 128 \
+	--residual_channels 32 \
+	--layer_size 2 \
+	--stack_size 2 \
+	--checkpoint_every 1 \
+	--accumulation_steps 10
+
+INFRA_OPTS?=--scratch_size 512 \
+	--memory 100
+
+DATASET_OPTS?=--datastore_name kinetics-all \
+	--datastore_version 1 \
+	--datastore_mount_dir /opt/datastore \
+
+.PHONY: train-gpu
+train-gpu:
+	grid run --dockerfile Dockerfile-gpu \
+		--instance_type p3.8xlarge \
+		--cpus 30  \
+		--gpus 4 \
+		--ignore_warnings \
+		${INFRA_OPTS} \
+		${DATASET_OPTS} \
+		movenet/trainer.py ${TRAIN_OPTS} \
+			--model_output_path models \
+			--pretrained_model_path "/artifacts/${PRETRAINED_RUN_EXP_NAME}/models/model.pth" \
+			--pretrained_run_exp_name "${PRETRAINED_RUN_EXP_NAME}" \
+			--grid_user_name="${GRID_USERNAME}" \
+			--grid_api_key="${GRID_API_KEY}" \
+			--wandb_api_key=${WANDB_API_KEY}
+
+
+.PHONY: train-gpu-spot
+train-gpu-spot:
+	grid run --dockerfile Dockerfile-gpu \
+		--instance_type p3.8xlarge \
+		--cpus 30  \
+		--gpus 4 \
+		--ignore_warnings \
+		--use_spot \
+		--auto_resume \
+		${INFRA_OPTS} \
+		${DATASET_OPTS} \
+		movenet/trainer.py ${TRAIN_OPTS} \
+			--model_output_path models \
+			--pretrained_model_path "/artifacts/${PRETRAINED_RUN_EXP_NAME}/models/model.pth" \
+			--pretrained_run_exp_name "${PRETRAINED_RUN_EXP_NAME}" \
+			--grid_user_name="${GRID_USERNAME}" \
+			--grid_api_key="${GRID_API_KEY}" \
+			--wandb_api_key=${WANDB_API_KEY}
