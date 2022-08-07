629352fffeab7d76847c28fe42ddfed135c88ec5
cosmicBboy <niels.bantilan@gmail.com>
2022-08-07 17:22:09 -0400

    use torch.tanh

--- movenet/modules.py
+++ movenet/modules.py
@@ -134,6 +134,6 @@ class DenseConv(nn.Module):
         self.conv2 = nn.Conv1d(out_channels, out_channels, 1)
 
     def forward(self, x):
-        x = self.conv1(F.tanh(x))
-        x = self.conv2(F.tanh(x))
+        x = self.conv1(torch.tanh(x))
+        x = self.conv2(torch.tanh(x))
         return F.softmax(x, dim=1)
